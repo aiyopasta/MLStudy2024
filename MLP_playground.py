Implemented Adagrad + fixed softmax NAN issue

MLP_playground.py
@@ -77,7 +77,7 @@ def A_many(vals):
 # TODO: Add a bit of jitter
 dataset = 0  # 0 = radial, 1 = affine, 2 = v-shaped
 x_train, y_train = [], []
-n_examples = 200
+n_examples = 150
 # Radial
 if dataset == 0:
     max_radius = height / 3.
@@ -222,7 +222,7 @@ def grad_check(x_input, y_input):
 
 # One step of training
 def train_step(x_input, y_input):
-    global X, X_normed, y, W_b, XW_b, Z, W2_b, ZW_b, Soft, Loss, accuracy
+    global X, X_normed, y, W_b, XW_b, Z, W2_b, ZW_b, Soft, Loss, accuracy, current_epoch
     X.set_input(x_input)  # In real ML problems, we'd iterate over mini-batches and set X's value to each mini-batch's value. 1 epoch = all mini-batches done.
     y.set_input(y_input)
     Loss.reset()
@@ -234,7 +234,11 @@ def train_step(x_input, y_input):
     # Update weights
     Loss.backfire()
     for param in [W_b, W2_b]:
-        param.update({'alpha': 0.0000002})
+        # param.update(params={'alpha': 0.0000001}, method='GD')
+        param.update(params={'alpha': 0.1}, method='adagrad')
+
+    # Print epochs, loss, accuracy
+    # print(current_epoch, Loss.value, accuracy)
 
 
 # Visualization parameters (for decision boundary visualization window and more)
@@ -333,13 +337,10 @@ def main():
         # break
 
         # Test some points (debugging)
-        # X.set_input(np.array([mouse_pos]))
-        # Loss.reset()
-        # Loss.fire()
-        # print(current_epoch, Soft.value)
-
-        # Print epochs, loss, accuracy
-        print(current_epoch, Loss.value, accuracy)
+        X.set_input(np.array([mouse_pos]))
+        Loss.reset()
+        Loss.fire()
+        print(current_epoch, Soft.value)
 
         # Essentially more epochs to the training if the data has changed
         if should_retrain:

computation_graph.py
@@ -59,6 +59,9 @@ class TensorNode:
         self.gradient = np.zeros(shape)
         self.learnable = learnable
 
+        # Additional params for certain optimization methids
+        self.cache = 0.0  # for adagrad
+
         self.name = name
 
     def set_input(self, tensor:np.ndarray):
@@ -93,6 +96,11 @@ class TensorNode:
             assert 'alpha' in params
             alpha = params['alpha']
             self.set_input(self.value - (alpha * self.gradient))
+        elif method == 'adagrad':
+            assert 'alpha' in params
+            alpha = params['alpha']
+            self.cache += np.sum(self.gradient * self.gradient)
+            self.set_input(self.value - ((alpha * self.gradient) / (np.sqrt(self.cache) + 1e-7)))
 
     def set_value_from_flat(self, new_val, flat_idx):
         assert self.learnable, str(self.name) + ' is not learnable, are you sure you want to be doing this?'
@@ -582,9 +590,12 @@ def relu_prime(tau):
 
 
 def softmax(Tau: np.array):
-    assert len(Tau.shape) == 2, 'input must be a matrix!'
-    exp = np.exp(Tau)
-    return exp / exp.sum(axis=1, keepdims=True)  # the sum results in a column vector, and '/' divides each row in exp by correponding element
+    assert len(Tau.shape) == 2, 'Input must be a matrix!'
+    maxVal = np.max(Tau, axis=1, keepdims=True)  # Find the max for each row
+    Tau_adjusted = Tau - maxVal  # Subtract max for numerical stability
+    exp = np.exp(Tau_adjusted)
+    return exp / np.sum(exp, axis=1, keepdims=True)
+
 
 
 # Single value softmax

sandbox.py
@@ -5,7 +5,7 @@ T = np.array([[-0.07426286,  0.12243876],
               [-0.0481759 , -0.0481759 ],
               [ 0.12243876, -0.07426286]])
 
-print(T.flatten('F'))
+print(T * T)
 
 # D = np.sum(T, axis=1, keepdims=True) * np.ones_like(T)
 # print(D)